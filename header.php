dd675b2875931c34c422c2316c03ea1c018c69d1
Waleed1520 <iqbalwaleed265@gmail.com>
2022-10-05 09:49:40 +0500

    smart point

---
+++ header.php
@@ -0,0 +1,65 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>smart point</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?> /style.css">
+	
+
+<?php wp_head(); ?>
+
+
+</head>
+<body>
+
+	
+
+
+ <div class="container-fluid">
+
+ 	<div class="row" id="Row1">
+
+<div class=" col-lg-3 col-md-12 col-sm-12 "  id="row1col3">
+	<center><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/logo.png"></a></center>
+
+</div>
+
+<div class=" col-lg-3 col-md-12 col-sm-12 " id="row2col3" >
+
+<center>	
+	<ul>
+		<li><a href="#">SERVICES</a></li>
+		<li><a href="#">CLIENTS</a></li>
+		<img src="<?php echo get_template_directory_uri(); ?>/vec.png">
+		
+	</ul>
+	</center>
+
+	
+	
+</div>
+
+
+
+<div class="col-lg-4 col-md-12 col-sm-12  " id="row3col4" >
+	<center><button id="button1">USERS</button>
+	<button id="button2">I WANT A SMART POINT</button>
+</center>
+</div>
+
+
+<div class="col-lg-2 col-md-12 col-sm-12 " id="row4col2" >
+	<center><img src="<?php echo get_template_directory_uri(); ?>/IMG2.png">
+	<img src="<?php echo get_template_directory_uri(); ?>/IMG3.png">
+<img src="<?php echo get_template_directory_uri(); ?>/vec.png"></center>
+</div>
+
+
+
+</div>
+
